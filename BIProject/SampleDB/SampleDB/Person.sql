8b8734d8579f42d915bbc02f07a3ae6a797524a6
Yazid Grim <mohamed.yazid.grim@gmail.com>
2015-12-29 11:01:50 -0500

    Create the DB Project
    Created a DB Project using VS2013 and the sqlproj project type. Added 2
    tables

---
+++ BIProject/SampleDB/SampleDB/Person.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[PersonID] INT NOT NULL PRIMARY KEY, 
+    [PersonFullName] NVARCHAR(150) NOT NULL, 
+    [BirthDate] DATE NOT NULL, 
+    [BirthCountry] NVARCHAR(150) NULL
+)
---
+++ BIProject/SampleDB/SampleDB/Store.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Store]
+(
+	[StoreID] INT NOT NULL PRIMARY KEY, 
+    [StoreName] NVARCHAR(150) NOT NULL, 
+    [Address] NVARCHAR(255) NOT NULL, 
+    [StoreManagerName] NVARCHAR(150) NOT NULL
+)

846f29db63609a5a7c92c7e3ae133de0efb8e8e9
Yazid Grim <mohamed.yazid.grim@gmail.com>
2015-12-29 15:15:30 -0500

    Adding deployment script for DB

---
+++ DeploymentScripts/PublishDatabase.ps1
@@ -0,0 +1,5 @@
+﻿<# Build the sqlproj #>
+& 'C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe' /t:publish /p:SqlPublishProfilePath="G:\Workspace\MSBI_Automation\BIProject\SampleDB\SampleDB\deployDB.publish.xml" 'G:\Workspace\MSBI_Automation\BIProject\SampleDB\SampleDB\SampleDB.sqlproj'
+
+<# Deploy to database server#>
+& 'C:\Program Files (x86)\Microsoft SQL Server\110\DAC\bin\SqlPackage.exe' /pr:"G:\Workspace\MSBI_Automation\BIProject\SampleDB\SampleDB\deployDB.publish.xml" /sf:"G:\Workspace\MSBI_Automation\BIProject\SampleDB\SampleDB\bin\output\SampleDB.dacpac" /a:Publish

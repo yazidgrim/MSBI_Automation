a884e893b4c7ac02a7689a7c9dda585c96463b82
Yazid Grim <mohamed.yazid.grim@gmail.com>
2015-12-29 14:08:48 -0500

    Added a column to store table

--- BIProject/SampleDB/SampleDB/Store.sql
+++ BIProject/SampleDB/SampleDB/Store.sql
@@ -3,5 +3,6 @@
 	[StoreID] INT NOT NULL PRIMARY KEY, 
     [StoreName] NVARCHAR(150) NOT NULL, 
     [Address] NVARCHAR(255) NOT NULL, 
-    [StoreManagerName] NVARCHAR(150) NOT NULL
+    [StoreManagerName] NVARCHAR(150) NOT NULL, 
+    [SurfaceInMsq] INT NULL
 )
